ff832dd3441f5fd2cdbd34459fa23a722df8de3f
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2021-11-21 14:01:14 +0100

    horizontal vertical

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+let token = true;
+
 // suppression comportement defaut clic droit
 document.addEventListener("contextmenu", function (event){
     event.preventDefault();
@@ -12,43 +14,70 @@ for (let i = 0; i < compartments.length ; i++) {
         console.log(event.button);
        switch (event.button){
            case 0 :
-               compartments [i].innerHTML = "X";
-               winner_horizontal();
-               winner_vertical();
+               if (token) {
+                   compartments [i].innerHTML = "X";
+                   winner();
+               }
                break;
            case 2 :
-               compartments [i].innerHTML = "O";
-               winner_horizontal();
-               winner_vertical();
+               if (token) {
+                   compartments [i].innerHTML = "O";
+                   winner();
+               }
                break;
        }
     })
 }
 
 // Vérification horizontale
-function winner_horizontal() {
-    if (compartments[0].innerHTML === compartments[1].innerHTML && compartments[0].innerHTML ===
-        compartments[2].innerHTML) {
-        alert("Tu a gagné");
+function winner() {
+    if (compartments[0].innerHTML === "O" || compartments[0].innerHTML === "X") {
+        if (compartments[0].innerHTML === compartments[1].innerHTML && compartments[0].innerHTML ===
+            compartments[2].innerHTML) {
+            token = false;
+            alert("Tu as gagné !");
         }
-        // else if (compartments[3].innerHTML === compartments[4].innerHTML && compartments[3].innerHTML ===
-        //     compartments[5].innerHTML) {
-        //     alert("Tu as gagné");
-        // }
-        // else if(compartments[6].innerHTML === compartments[7].innerHTML && compartments[6].innerHTML ===
-        //     compartments[8].innerHTML) {
-        // alert("Tu as gagné");
-        // }
-}
-
-// Vérification vertical
-function winner_vertical() {
-    if (compartments[0].innerHTML === compartments[4].innerHTML && compartments[0].innerHTML ===
-        compartments[8].innerHTML){
-        alert("Tu as gagné");
+        else if (compartments[0].innerHTML === compartments[3].innerHTML && compartments[0].innerHTML ===
+        compartments[6].innerHTML) {
+            token = false;
+            alert("Tu as gagné !");
+        }
+        else if (compartments[0].innerHTML === compartments[4].innerHTML && compartments[0].innerHTML ===
+            compartments[8].innerHTML) {
+            token = false;
+            alert("Tu as gagné !");
+        }
+    }
+    else if (compartments[4].innerHTML === "O" || compartments[4].innerHTML === "X") {
+        if (compartments[6].innerHTML === compartments[4].innerHTML && compartments[4].innerHTML ===
+            compartments[3].innerHTML) {
+            token = false;
+            alert("Tu as gagné !");
+        }
+        else if (compartments[5].innerHTML === compartments[2].innerHTML && compartments[5].innerHTML ===
+            compartments[7].innerHTML) {
+            token = false;
+            alert("Tu as gagné !");
+        }
+        else if (compartments[2].innerHTML === compartments[4].innerHTML && compartments[4].innerHTML ===
+            compartments[6].innerHTML) {
+            token = false;
+            alert("Tu as gagné !")
+        }
+    }
+    else if (compartments[8].innerHTML === "O" || compartments[8].innerHTML === "X") {
+        if (compartments[8].innerHTML === compartments[2].innerHTML && compartments[8].innerHTML ===
+            compartments[6].innerHTML) {
+            token = false;
+            alert("Tu as gagné !");
+        }
+        else if (compartments[8].innerHTML === compartments[6].innerHTML && compartments[8].innerHTML ===
+            compartments[7].innerHTML) {
+            token = false;
+            alert("Tu as gagné !");
+        }
     }
 }
-
 
 let retry = document.getElementById("submit");
 
