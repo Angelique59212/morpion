d436baca81d3d5f3117746aeb7db818b4ed967d7
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2021-11-22 11:13:59 +0100

    morpion

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
 let token = true;
 
-// suppression comportement defaut clic droit
+// remove right click
 document.addEventListener("contextmenu", function (event){
     event.preventDefault();
 });
 
-// Appel des divs
+// call div
 let compartments = document.getElementsByClassName("compartment");
+let win = document.getElementById("winner");
+let equal = document.getElementById("equality");
 
-//désignation joueur 1 et 2
+//player designation 1 and 2
 for (let i = 0; i < compartments.length ; i++) {
     compartments [i].addEventListener("mouseup", function (event){
-        console.log(event.button);
        switch (event.button){
            case 0 :
                if (token) {
@@ -29,58 +30,76 @@ for (let i = 0; i < compartments.length ; i++) {
     })
 }
 
-// Vérification horizontale
+// winner verification
 function winner() {
     if (compartments[0].innerHTML === "O" || compartments[0].innerHTML === "X") {
         if (compartments[0].innerHTML === compartments[1].innerHTML && compartments[0].innerHTML ===
             compartments[2].innerHTML) {
             token = false;
-            alert("Tu as gagné !");
+            win.innerHTML = "Tu as gagné";
         }
         else if (compartments[0].innerHTML === compartments[3].innerHTML && compartments[0].innerHTML ===
         compartments[6].innerHTML) {
             token = false;
-            alert("Tu as gagné !");
+            win.innerHTML = "Tu as gagné";
         }
         else if (compartments[0].innerHTML === compartments[4].innerHTML && compartments[0].innerHTML ===
             compartments[8].innerHTML) {
             token = false;
-            alert("Tu as gagné !");
+            win.innerHTML = "Tu as gagné";
         }
     }
     else if (compartments[4].innerHTML === "O" || compartments[4].innerHTML === "X") {
-        if (compartments[6].innerHTML === compartments[4].innerHTML && compartments[4].innerHTML ===
+        if (compartments[6].innerHTML === compartments[4].innerHTML && compartments[6].innerHTML ===
+            compartments[2].innerHTML) {
+            token = false;
+            win.innerHTML = "Tu as gagné";
+        }
+        else if (compartments[5].innerHTML === compartments[4].innerHTML && compartments[5].innerHTML ===
             compartments[3].innerHTML) {
             token = false;
-            alert("Tu as gagné !");
-        }
-        else if (compartments[5].innerHTML === compartments[2].innerHTML && compartments[5].innerHTML ===
-            compartments[7].innerHTML) {
-            token = false;
-            alert("Tu as gagné !");
+            win.innerHTML = "Tu as gagné";
         }
         else if (compartments[2].innerHTML === compartments[4].innerHTML && compartments[4].innerHTML ===
             compartments[6].innerHTML) {
             token = false;
-            alert("Tu as gagné !")
+            win.innerHTML = "Tu as gagné";
+        }
+        else if (compartments[1].innerHTML === compartments[4].innerHTML && compartments[1].innerHTML ===
+            compartments[7].innerHTML) {
+            win.innerHTML = "Tu as gagné";
         }
     }
     else if (compartments[8].innerHTML === "O" || compartments[8].innerHTML === "X") {
         if (compartments[8].innerHTML === compartments[2].innerHTML && compartments[8].innerHTML ===
-            compartments[6].innerHTML) {
+            compartments[5].innerHTML) {
             token = false;
-            alert("Tu as gagné !");
+            win.innerHTML = "Tu as gagné";
         }
         else if (compartments[8].innerHTML === compartments[6].innerHTML && compartments[8].innerHTML ===
             compartments[7].innerHTML) {
             token = false;
-            alert("Tu as gagné !");
+            win.innerHTML = "Tu as gagné";
         }
     }
+
+        let nb = 0;
+        for (let i = 0; i < 9; i++) {
+            if (compartments[i].innerHTML === ""){
+                break;
+            }
+            else {
+                nb++;
+            }
+        }
+        if (nb === 9) {
+            equal.innerHTML = "Egalité";
+        }
 }
 
+//replay
 let retry = document.getElementById("submit");
-
 retry.addEventListener("click", function (){
-
+    token = true;
+    win.innerHTML = "";
 })
